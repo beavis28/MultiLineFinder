e047a0e1531055f2358efe79fe68167cb47ec89a
beavis28 <satoshiho2012@gmail.com>
2014-09-28 12:33:19 +0800

    last modify

--- popup.js
+++ popup.js
@@ -8,12 +8,12 @@ function takeComparison(ts, s)
 	var lines = ts.split("\n");
 	for(var i = 0; i < lines.length; i ++)
 	{
-		var line = lines[i];
+		var line = lines[i].trim();
 		if (line != null)
 		{
 			var re = new RegExp(line, "i");
-			var ret = s.match(re);
-			if (ret != null)
+			var ret = s.search(re);
+			if (ret != -1)
 			{
 				result = result + line + "\n";
 			}
@@ -25,7 +25,7 @@ chrome.extension.onMessage.addListener(function(request, sender) {
 	  if (request.action == "getSource") {
 		  contents = request.source;
 	      takeComparison(text_area, contents);
-	      document.write(contents);
+	      document.write(result);
 	  }
 	});
 
